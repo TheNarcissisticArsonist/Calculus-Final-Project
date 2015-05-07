497fa6ff9f29deff6108264cebdbc0f015b527bc
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-05-07 16:51:25 -0400

    Implemented method 2

--- Main.js
+++ Main.js
@@ -46,3 +46,46 @@ function m1NextPrime() {
   primeList.innerHTML = primes.join(", ");
   currentPrime.innerHTML = primes.length;
 }
+
+//Method 2:
+document.getElementById("m21").addEventListener("click", m2NextPrime);
+document.getElementById("m22").addEventListener("click", function() {
+  num = prompt("How many primes would you like to generate?");
+  if(isNaN(num)) {
+    return;
+  }
+  for(j=0; j<num; j++) {
+    m2NextPrime();
+  }
+});
+document.getElementById("m23").addEventListener("click", function() {
+  maxNum = prompt("Up to what number of primes would you like to generate?");
+  if(isNaN(maxNum)) {
+    return;
+  }
+  currentNum = currentPrime.innerHTML;
+  num = maxNum - currentNum;
+  for(j=0; j<num; j++) {
+    m2NextPrime();
+  }
+});
+function m2NextPrime() {
+  currentNum = primes[primes.length - 1];
+  foundNext = false;
+  while(!foundNext) {
+    currentNum++;
+    broke = false;
+    for(i=0; i<primes.length; i++) {
+      if(currentNum % primes[i] == 0) {
+        broke = true;
+        break;
+      }
+    }
+    if(!broke) {
+      foundNext = true;
+      primes.push(currentNum);
+    }
+  }
+  primeList.innerHTML = primes.join(", ");
+  currentPrime.innerHTML = primes.length;
+}
